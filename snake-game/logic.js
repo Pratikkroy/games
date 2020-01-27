3e1aff81bc999850d0a5f0899e83f7bc21b7c843
Pratik Roy <roypra@amazon.com>
2020-01-28 00:21:49 +0530

    updated the snake game

--- snake-game/logic.js
+++ snake-game/logic.js
@@ -2,16 +2,18 @@
 
 var canvasX = 0, canvasY = 0;
 var canvasWidth = 800, canvasHeight = 620;
-var canvasPaddindLeft = 9, canvasPaddindTop = 10;
+var canvasPaddingLeft = 10, canvasPaddingTop = 10;
 var canvasBaseColor = 'black'
 
-var ballX = 60 + canvasPaddindLeft, ballY = 100 + canvasPaddindTop;
+var ballX = 60 + canvasPaddingLeft, ballY = 100 + canvasPaddingTop;
+var ballCoordinates;
+var oldBallCoordinates = [];
 var ballRadius = 10;
 var ballColor = 'red';
 
 var snakeCoordsArr = [];
-var snakeHeadPositionX = 400 + canvasPaddindLeft;
-var snakeHeadPositionY = 300 + canvasPaddindTop;
+var snakeHeadPositionX = 400 + canvasPaddingLeft;
+var snakeHeadPositionY = 300 + canvasPaddingTop;
 var snakeLength = 5;
 var snakePerUnitLength = 2 * ballRadius;
 var snakePerUnitWidth = 2 * ballRadius;
@@ -48,29 +50,13 @@ function main() {
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
 
+    // initialise the ball Coordinates
+    ballCoordinates = new Coordinates(ballX, ballY);
 
     // support animation
     initialiseSnakeArray();
     startGame();
 
-}
-
-function firstRacketMovement() {
-    var mousePosition = calculateMousePosition(event);
-    var mousePositionY = mousePosition.y;
-    racket1Y = mousePositionY - racketHeight / 2;
-}
-
-function calculateMousePosition(event) {
-    var rectangle = canvas.getBoundingClientRect();
-    var root = document.documentElement;
-    var mouseX = event.clientX - rectangle.left - root.scrollLeft;
-    var mouseY = event.clientY - rectangle.top - root.scrollTop;
-
-    return {
-        x: mouseX,
-        y: mouseY
-    }
 }
 
 function initialiseSnakeArray() {
@@ -92,7 +78,7 @@ function startGame() {
 }
 
 function keyDown(event) {
-    console.log(event)
+    // console.log(event)
     if (!isGameStopped) {
         switch (event.code) {
             case 'ArrowUp':
@@ -117,7 +103,7 @@ function keyDown(event) {
 
 function keyPress(event) {
 
-    console.log(event)
+    // console.log(event)
     if (event.code === "Space") {
         if (isGameStopped) {
             startGame();
@@ -138,34 +124,56 @@ function keyPress(event) {
 }
 
 function resetGame() {
-    console.log("Reset")
+    console.log("Reset");
+
+
 }
 
+function stopGame() {
+    isGameStopped = true;
+    clearInterval(refreshIntervalId);
+}
 
-/* --------------------- code for moving ball ------------------------ */
+/* --------------------- code for moving snake ------------------------ */
 function move() {
 
+    // check if the snake hit the boundary or itself.
+    if(checkIfSnakeHitTheBoundary() || checkIfSnakeHitItself()){
+        stopGame();
+    }
+    
+    // move snake one step forward
+    shiftSnakeCoordsArrRight();
     switch (snakeDirection) {
         case 'UP':
-            shiftSnakeCoordsArrRight();
             snakeHeadPositionY -= snakePerUnitLength;
-            snakeCoordsArr[0] = new Coordinates(snakeHeadPositionX, snakeHeadPositionY);
             break;
         case 'RIGHT':
-            shiftSnakeCoordsArrRight();
             snakeHeadPositionX += snakePerUnitWidth;
-            snakeCoordsArr[0] = new Coordinates(snakeHeadPositionX, snakeHeadPositionY);
             break;
         case 'DOWN':
-            shiftSnakeCoordsArrRight();
             snakeHeadPositionY += snakePerUnitLength;
-            snakeCoordsArr[0] = new Coordinates(snakeHeadPositionX, snakeHeadPositionY);
             break;
         case 'LEFT':
-            shiftSnakeCoordsArrRight();
             snakeHeadPositionX -= snakePerUnitWidth;
-            snakeCoordsArr[0] = new Coordinates(snakeHeadPositionX, snakeHeadPositionY);
             break;
+    }
+    snakeCoordsArr[0] = new Coordinates(snakeHeadPositionX, snakeHeadPositionY);
+
+    // check if snake hits the ball 
+    // if yes place ball at new place and increase the snake length
+    if(checkIfSnakeHitTheBall()){
+        oldBallCoordinates.push(ballCoordinates);
+        ballCoordinates = calculateNewPositionOfBall();
+        playerScore += 1;
+        // snakeCoordsArr.push(new Coordinates(oldBallCoordinates.X, oldBallCoordinates.Y));
+        // snakeLength += 1;
+    }
+
+    if(oldBallCoordinates.length>0 && snakeCoordsArr[snakeLength-1].X == oldBallCoordinates[0].X && snakeCoordsArr[snakeLength-1].Y == oldBallCoordinates[0].Y){
+        snakeCoordsArr.push(new Coordinates(oldBallCoordinates[0].X, oldBallCoordinates[0].Y));
+        snakeLength += 1;
+        oldBallCoordinates.shift();
     }
 }
 
@@ -175,10 +183,71 @@ function shiftSnakeCoordsArrRight() {
     }
 }
 
+function checkIfSnakeHitTheBoundary(){
+    if(snakeCoordsArr[0].X<canvasPaddingLeft 
+        || snakeCoordsArr[0].X>=canvasPaddingLeft + canvasWidth
+        || snakeCoordsArr[0].Y<canvasPaddingTop
+        || snakeCoordsArr[0].Y>=canvasPaddingTop + canvasHeight){
+        return true;
+    }
+    return false;
+}
 
+function checkIfSnakeHitItself(){
+    for(var i=1; i <= snakeLength-1; i++){
+        if(snakeCoordsArr[0].X == snakeCoordsArr[i].X 
+            && snakeCoordsArr[0].Y == snakeCoordsArr[i].Y){
+                return true;
+            }            
+    }
+    return false;
+}
 
+function checkIfSnakeHitTheBall(){
+    if(snakeCoordsArr[0].X == ballCoordinates.X && snakeCoordsArr[0].Y == ballCoordinates.Y){
+        return true;
+    }
+    return false;
+}
 
+function calculateNewPositionOfBall(){
+    do{
+        var newBallCoordinatesX = getRandomIntegerNumber(canvasPaddingLeft, canvasPaddingLeft+canvasWidth);
+        var newBallCoordinatesY = getRandomIntegerNumber(canvasPaddingTop, canvasPaddingTop+canvasHeight);
+    }while(checkNewBallPosition(newBallCoordinatesX, newBallCoordinatesY));
+    
+    return new Coordinates(newBallCoordinatesX, newBallCoordinatesY);
+}
 
+function getRandomIntegerNumber(min, max){ 
+    var ballSquareDimension = 2* ballRadius;
+    return min + Math.floor(Math.random()*((max-min)/ballSquareDimension))*ballSquareDimension;
+}
+
+function checkNewBallPosition(newBallCoordinatesX, newBallCoordinatesY){
+
+    // check if new ball position is on the previous position
+    if(ballCoordinates.X == newBallCoordinatesX && ballCoordinates.Y == newBallCoordinatesY){
+        return true;
+    }
+
+    // check if new ball position is on the snake
+    for(var i = 0; i < snakeLength; i++){
+        if(newBallCoordinatesX == snakeCoordsArr[i].X && newBallCoordinatesY == snakeCoordsArr[i].Y){
+            return true;
+        }
+    }
+
+    // check if new ball position is out of canvas
+    if(newBallCoordinatesX<canvasPaddingLeft 
+        || newBallCoordinatesX>=canvasPaddingLeft + canvasWidth
+        || newBallCoordinatesY<canvasPaddingTop
+        || newBallCoordinatesY>=canvasPaddingTop + canvasHeight){
+        return true;
+    }
+
+    return false;
+}
 
 /* --------------------- code for drawing ------------------------ */
 
@@ -192,7 +261,7 @@ function draw() {
     // draw ball
     canvasContext.fillStyle = ballColor;
     canvasContext.beginPath();                      // since there is no fillArc method
-    canvasContext.arc(ballX, ballY, ballRadius, 0, 2 * Math.PI, true);
+    canvasContext.arc(ballCoordinates.X, ballCoordinates.Y, ballRadius, 0, 2 * Math.PI, true);
     canvasContext.fill();
 
     // draw snake
@@ -202,6 +271,12 @@ function draw() {
         canvasContext.arc(snakeCoordsArr[i].X, snakeCoordsArr[i].Y, ballRadius, 0, 2 * Math.PI, true);
         canvasContext.fill();
     }
+
+    // show score 
+    document.getElementById("score").innerHTML = playerScore;
+
+    // canvasContext.fillStyle = 'white';
+    // canvasContext.fillText(playerScore,canvasX+100,100);
 }
 
 
